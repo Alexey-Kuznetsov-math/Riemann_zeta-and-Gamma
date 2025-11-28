ace9bb7b4b98f9c4877aaa51b8077f8f6ce96530
Alexey-Kuznetsov-math <alexey.kuznetsov@gmail.com>
2025-11-28 18:48:24 -0500

    Create Riemann_zeta.m

---
+++ Riemann_zeta.m
@@ -0,0 +1,224 @@
+function f=Riemann_zeta(s)
+% Riemann_zeta approximates the Riemann zeta function in the entire complex plane
+%
+%   f = Riemann_zeta(s) returns an approximation to zeta(s) for complex input s
+%   The input s can be a scalar or vector
+%
+% For |Im(s)|<100 the approximation is correct to around 13 decimal digits;
+% For |Im(s)|<1000 the approximation is correct to around 12 decimal digits;
+% For |Im(s)|<10000 the approximation is correct to around 11 decimal digits;
+% For larger values of |Im(s)| the accuracy will continue to decrease in a similar way: 
+% with every increase of Im(s) by a factor of ten we lose approximately one decimal digit of precision. 
+% More details can be found at the end of Section 1 in [1].
+%
+% When |Im(s)|>200 and -4<Re(s)<5 we use the approximation zeta_8(s) developed in [1]. 
+% For other values of s we use either Euler-Maclaurin formula or direct summation zeta(s)=\sum_{n=1}^{\infty} n^{-s}.
+% -------------------------------------------------------------------------
+% Author: Alexey Kuznetsov
+% York University, Toronto, Canada
+% Website: https://kuznetsovmath.ca/
+% Email: akuznets@yorku.ca
+%
+% Created: 18-Nov-2025
+% Last updated: 18-Nov-2025
+%
+% References:
+% [1] A. Kuznetsov, "Simple and accurate approximations to the Riemann zeta function", 2025, 
+% preprint, https://arxiv.org/abs/2503.09519
+%
+% License: BSD 3-Clause (https://opensource.org/licenses/BSD-3-Clause)
+%--------------------------------------------------------------------------
+    i1=find(real(s)>=0);
+    i2=find((real(s)<0)&(imag(s)>=0));
+    i3=find((real(s)<0)&(imag(s)<0));
+    f=zeros(size(s));
+    if (~isempty(i1)) 
+        f(i1)=Riemann_zeta_half_plane(s(i1));
+    end
+    if (~isempty(i2)) % use reflection formula if Re(s)<0
+        w2=s(i2);
+        f(i2)=1i*(2*pi).^(w2-1).*(1-exp(1i*pi*w2)).*exp(-0.5i*pi*w2+ln_gamma(1-w2)).*Riemann_zeta_half_plane(1-w2);
+    end
+    if (~isempty(i3)) % use reflection formula if Re(s)<0
+        w3=s(i3);
+        f(i3)=-1i*(2*pi).^(w3-1).*(1-exp(-1i*pi*w3)).*exp(0.5i*pi*w3+ln_gamma(1-w3)).*Riemann_zeta_half_plane(1-w3);
+    end
+end
+%##########################################################################
+function f=Riemann_zeta_half_plane(s)
+% approximate zeta(s) in the half-plane Re(s)>0
+%--------------------------------------------------------------------------
+    i1=find(real(s)>=5);
+    i2=find((real(s)<5)&(abs(imag(s))<=200));
+    i3=find((real(s)<5)&(imag(s)>200));
+    i4=find((real(s)<5)&(imag(s)<-200)); 
+    f=zeros(size(s));
+    if (~isempty(i1)) % in the half-plane Re(s)>=5 we compute zeta(s) by summing \sum_{k=1}^{\infty} k^(-s) 
+        f(i1)=zeta_main_sum(s(i1));
+    end
+    if (~isempty(i2)) % if 0<Re(s)<5 and |Im(s)|<=200, we use Euler-Maclaurin method to compute zeta(s)
+        f(i2)=zeta_Euler_Maclaurin(s(i2));
+    end
+    if (~isempty(i3)) % if 0<Re(s)<5 and Im(s)>200, we use zeta_8(s) approximation
+        f(i3)=zeta_8(s(i3));
+    end
+    if (~isempty(i4)) % if 0<Re(s)<5 and Im(s)<-200, we use zeta_8(s) approximation
+        f(i4)=conj(zeta_8(conj(s(i4))));
+    end 
+end   
+%##########################################################################
+function f=zeta_8(s)
+% zeta_8 computes the approximation to the Riemann zeta function described in [1] 
+% References:
+% [1] A. Kuznetsov, "Simple and accurate approximations to the Riemann zeta function", 2025, 
+% preprint, https://arxiv.org/abs/2503.09519
+%--------------------------------------------------------------------------
+% the cofficients lambda_j for j=1,2,...,8
+    lambda=[0.152845417613666702426-0.119440685603870510384i
+            0.302346225128945757427-0.243989695504400621268i          
+	        0.451119584531782942888-0.378479770209444563858i 
+	        0.604563710297226464637-0.523486888629095259770i
+	        0.765965706759629396959-0.678405572413543444272i
+	        0.938371150977889047740-0.845332361280975174880i
+	        1.128148837845288402558-1.030737947568157685685i
+	        1.353030558654668162533-1.252503278108132307164i];               
+    % the cofficients omega_j for j=0,1,2,...,8
+    omega0=1.926019633029103199063e-1+2.472986965795651842299e-2i;
+    omega=[1.582954327321094104502e-1+4.149113569204600502105e-2i   
+	       7.826728293587305110862e-2+5.215518667623989653254e-2i   
+	       1.940595049247490540621e-2+2.977286598777633378610e-2i  
+	       1.691184771902755036966e-3+8.938933548999206800196e-3i     
+	      -2.994777986686168319731e-4+1.567541981830224487301e-3i   
+	      -9.837202592542590210980e-5+1.502108057352792742070e-4i  
+          -9.346989286415688998740e-6+5.793852209955845432028e-6i  
+	      -2.451577304299235983015e-7+6.134784898751456953524e-9i]; 
+%--------------------------------------------------------------------------
+    if (length(s)==1) 
+        % compute chi(s)=(2*pi)^s/(2*cos(pi*s/2)*gamma(s))
+        % we use Stirling's formula for log(gamma(s)) and truncate
+        % the asymptotic series \sum_{n\ge 1} B_{2n}/(2*n*(2*n-1)*s^(2*n-1)) at n=3
+        chi=exp((0.5-s)*log(s/(2*pi))+s*(1+0.5i*pi)-(1/s)*(1/12+s^(-2)*(-1/360+s^(-2)/1260)));
+        N=floor(sqrt(imag(s)/(2*pi)));
+        M=N+0.5;
+        lnn=log(2:N);
+        f=1+sum(exp(-s*lnn))+chi*(1+sum(exp((s-1)*lnn))); %compute the main sum 
+        I1=exp(-s*log(M))*(omega0+sum(omega.*(exp(-2*pi*M*lambda-s*log(1+1i*lambda/M))+exp(2*pi*M*lambda-s*log(1-1i*lambda/M)))));
+        s1=1-conj(s);
+        I2=conj(exp(-s1*log(M))*(omega0+sum(omega.*(exp(-2*pi*M*lambda-s1*log(1+1i*lambda/M))+exp(2*pi*M*lambda-s1*log(1-1i*lambda/M))))));
+        f=f-0.5*(-1)^N*(I1+chi*I2); 
+    else
+        chi=exp((0.5-s).*log(s/(2*pi))+s*(1+0.5i*pi)-(1./s).*(1/12+s.^(-2).*(-1/360+s.^(-2)/1260)));
+        f=1+chi;
+        N=floor(sqrt(imag(s)/(2*pi)));
+        for n=2:max(N)  %compute the main sum 
+            f=f+(n<=N).*(exp(-s*log(n))+chi.*exp((s-1)*log(n))); 
+        end
+        M=N+0.5;
+        s1=1-conj(s);
+        I1=omega0*ones(size(s));
+        I2=I1;
+        for j=1:8
+            I1=I1+omega(j)*(exp(-2*pi*M*lambda(j)-s.*log(1+1i*lambda(j)./M))+exp(2*pi*M*lambda(j)-s.*log(1-1i*lambda(j)./M)));
+            I2=I2+omega(j)*(exp(-2*pi*M*lambda(j)-s1.*log(1+1i*lambda(j)./M))+exp(2*pi*M*lambda(j)-s1.*log(1-1i*lambda(j)./M)));
+        end
+        I1=exp(-s.*log(M)).*I1;
+        I2=conj(exp(-s1.*log(M)).*I2);
+        f=f-0.5*(-1).^N.*(I1+chi.*I2); 
+    end
+end
+%##########################################################################
+function f=zeta_main_sum(s)
+% computes zeta(s)=\sum_{n=1}^{\infty} n^{-s}  
+% We remove from this sum all numbers divisible by 2,3,5,7,11,13,17,19 and truncate the resulting sum at n=500
+% This results in a more efficient algorithm (fewer terms in the main sum)
+% This function gives a good approximation to zeta(s) in the half-plane Re(s)>5
+    n=[23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 ...
+       179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 ...
+       347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499];
+    f=ones(size(s));
+    for k=1:87
+        f=f+n(k).^(-s);
+    end
+    f=f./((1-2.^(-s)).*(1-3.^(-s)).*(1-5.^(-s)).*(1-7.^(-s)).*(1-11.^(-s)).*(1-13.^(-s)).*(1-17.^(-s)).*(1-19.^(-s)));
+end
+%##########################################################################
+function f=zeta_Euler_Maclaurin(s)
+% computes zeta(s) via Euler-Maclaurin summation 
+% we use formula (25.2.10) at https://dlmf.nist.gov/25.2 with N=200 and n=9
+%--------------------------------------------------------------------------
+% precompute b(k)=B_{2k}/(2k)!, where B_{k} are Bernoulli numbers
+    b=[8.33333333333333e-2,-1.38888888888889e-3,3.306878306878306e-05,-8.267195767195768e-07,2.087675698786810e-08, ...
+      -5.284190138687493e-10,1.338253653068468e-11,-3.389680296322583e-13,8.586062056277845e-15];
+    N=200;
+    m=s/N;
+    f=zeros(size(s));
+    for k=1:9
+        f=f+b(k)*m;
+        m=m.*(s+2*k-1).*(s+2*k)/N^2;
+    end
+    f=1+N.^(-s).*(f+0.5+N./(s-1));
+    for k=2:(N-1)
+        f=f+k.^(-s);
+    end
+end
+%##########################################################################
+function f=ln_gamma(z)
+% computes the logarithm of the Gamma function in the entire complex plane.
+% References:
+% [1] A. Kuznetsov, "Computing the Barnes G-function and the gamma function
+%     in the entire complex plane", Journal of Computational and Applied Mathematics,
+%     Vol. 411, 2022, 114270. https://doi.org/10.1016/j.cam.2022.114270
+%
+% [2] A. Kuznetsov, A. Mohammadioroojeh, "Approximating functions on R^+
+%     by exponential sums", 2025, preprint, https://arxiv.org/abs/2508.19095
+% -------------------------------------------------------------------------
+    i1=find(real(z)>=1.5);
+    i2=find((real(z)>=0.5)&(real(z)<1.5));
+    i3=find((real(z)<0.5)&(imag(z)>=0));
+    i4=find((real(z)<0.5)&(imag(z)<0));
+    f=zeros(size(z));
+    if (~isempty(i1))
+        f(i1)=ln_gamma_half_plane(z(i1));
+    end
+    if (~isempty(i2)) % use functional equation log(Gamma(z))=log(Gamma(z+1))-log(z)
+        f(i2)=ln_gamma_half_plane(z(i2)+1)-log(z(i2));
+    end
+    if (~isempty(i3))  % use reflection formula
+        w3=z(i3);
+        f(i3)=log(1-w3)-ln_gamma_half_plane(2-w3)+1.83787706640935+1i*pi*(w3-0.5)-log(1-exp(2i*pi*w3));
+    end
+    if (~isempty(i4))  % use reflection formula
+        w4=z(i4);
+        f(i4)=log(1-w4)-ln_gamma_half_plane(2-w4)+1.83787706640935-1i*pi*(w4-0.5)-log(1-exp(-2i*pi*w4));
+    end
+end
+%##########################################################################
+function f=ln_gamma_half_plane(z)
+% approximates the logarithm of the Gamma function in the half-plane Re(z)>=1.5
+% -------------------------------------------------------------------------
+    c=[-5.3035486658210424e-9+8.47546314707099368e-10i
+        1.0093321787093660e-6+2.72761804689954947e-7i
+       -2.9946104278440048e-5+3.68867702224184848e-5i
+        5.1324321142829496e-4+1.31127415401525033e-3i];
+    l=[3.036399609619316394+2.183538146270238962i
+       2.619619673091566707+1.285513733113457465i 
+       2.236468161644125811-6.451943678012780725e-01i
+       1.822691064348029853+1.260996493971840621e-01i];
+    cr=[ 1.91267259422448627e-12
+        -1.34922683153592188e-3
+        -8.28182554958193984e-4
+        -1.80081775867242537e-4];
+    lr=[7.862008471885223854
+        1.278757134063109466   
+        1.129114403699685475   
+        1.037917494906115128];
+    f=zeros(size(z));
+    for j=1:4
+        f=f+c(j)./(z-1+l(j)).^3+conj(c(j))./(z-1+conj(l(j))).^3+cr(j)./(z-1+lr(j)).^3;
+    end
+    f=2*f+0.918938533204673+(z-0.5).*log(z)-z+0.083333333333333./z;
+end
+%##########################################################################
+
+
+
