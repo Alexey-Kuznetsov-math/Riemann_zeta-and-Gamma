0c0140f2161e4b85cd16e1a913a6b7d08b1762ae
Alexey Kuznetsov <alexey.kuznetsov@gmail.com>
2025-12-05 08:16:20 -0500

    Update Riemann_zeta.f90

--- Fortran/Riemann_zeta.f90
+++ Fortran/Riemann_zeta.f90
@@ -42,7 +42,7 @@
 	complex (kind=16)		:: f
 	real(kind=16)			:: N_sum, N_EM, N_12
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
-	N_sum=8*exp(75/(s%re-1))/30  ! computational complexity of zeta_summation function -- counting the number of evaluations of k^{-s}
+	N_sum=8*exp(75/max(s%re-1,0.5q0))/30  ! computational complexity of zeta_summation function -- counting the number of evaluations of k^{-s}
 	N_EM=abs(s)/2+20		    ! computational complexity of zeta_Euler_Maclaurin function			
 	N_12=sqrt(abs(s%im)/(2*pi_16))/2+122	! computational complexity of zeta_12 function
 	if (abs(s%im)<200) then 
